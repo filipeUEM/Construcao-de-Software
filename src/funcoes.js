6311bc4fac46b18e12e7953c86f09cc2db1987df
Gustavo Tadashi Takehara <gustavotakehara@gmail.com>
2024-03-17 14:40:25 -0300

    cria js de funcoes

---
+++ src/funcoes.js
@@ -0,0 +1,8 @@
+//prefixes of implementation that we want to test
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+
+//prefixes of window.IDB objects
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+window.IDBKeyRange    = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+
+if (!window.indexedDB) window.alert("O seu navegador nao suporta uma versao estavel do IndexedDB.");
